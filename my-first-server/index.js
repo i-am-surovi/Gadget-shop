d3a66d08e5221e7d8c4470c65ee5a25ea0c63fa6
i-am-surovi <ssur.ovi03@gmail.com>
2024-11-23 23:53:45 +0600

    server file added

---
+++ my-first-server/index.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// mongodb
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rew54.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(url, {
+    serverApi:{
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+})
+
+const dbConnect = async()=>{
+    try{
+        client.connect();
+        console.log("Database connected successfully");
+    }catch(error){
+        console.log(error.name, error.message);
+    }
+}
+dbConnect()
+
+// api
+
+app.get("/", (req,res) => {
+    res.send("Server is running");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port, ${port}`);
+})
